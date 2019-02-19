f74ea674ce1ce49685ed41c11df764c94c0cd618
CakeZinito <icake.cc@gmail.com>
2019-02-19 12:47:44 +0100

    permissions

--- app/src/main/java/com/example/thetrashmap/MainActivity.java
+++ app/src/main/java/com/example/thetrashmap/MainActivity.java
@@ -1,4 +1,4 @@
-package edmt.dev.androidcamera2api;
+package com.example.thetrashmap;
 
 import android.Manifest;
 import android.content.Context;
@@ -16,13 +16,13 @@ import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.Image;
 import android.media.ImageReader;
+import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Size;
 import android.util.SparseIntArray;
 import android.view.Surface;
@@ -30,6 +30,8 @@ import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.example.thetrashmap.R;
 
 import java.io.File;
 import java.io.FileNotFoundException;
